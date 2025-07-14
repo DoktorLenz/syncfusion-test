70ca769f21f7ba4031d67cde3431ff3bd2cd2d2c
Laurenz Stinner <laurenz.stinner@oneqrew.com>
2025-07-14 16:44:58 +0200

    init

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,72 @@
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  AnnotationService,
+  BookmarkViewService,
+  FormDesignerService,
+  FormFieldsService,
+  LinkAnnotationService,
+  MagnificationService,
+  NavigationService,
+  PageOrganizerService,
+  PdfViewerComponent,
+  PdfViewerModule,
+  PrintService,
+  TextSearchService,
+  TextSelectionService,
+  ThumbnailViewService,
+  ToolbarService,
+} from '@syncfusion/ej2-angular-pdfviewer';
+
+@Component({
+  selector: 'app-test',
+  imports: [PdfViewerModule],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css',
+  providers: [
+    LinkAnnotationService,
+    BookmarkViewService,
+    MagnificationService,
+    ThumbnailViewService,
+    ToolbarService,
+    NavigationService,
+    TextSearchService,
+    TextSelectionService,
+    PrintService,
+    AnnotationService,
+    FormDesignerService,
+    FormFieldsService,
+    PageOrganizerService,
+  ],
+})
+export class TestComponent implements OnDestroy {
+  @ViewChild('pdfViewer') pdfViewer: PdfViewerComponent | undefined;
+
+  public document: string =
+    'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+  public resource: string = window.location.origin + '/ej2-pdfviewer-lib';
+
+  changeDocument() {
+    this.pdfViewer?.unload();
+    if (this.document.startsWith('https')) {
+      this.document = window.location.origin + '/_pkpadmin,+1008-4741-1-CE.pdf';
+    } else {
+      this.document =
+        'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.pdfViewer) {
+      if (this.pdfViewer) {
+        this.pdfViewer?.unload();
+        this.pdfViewer?.destroy();
+      }
+    }
+  }
+}
